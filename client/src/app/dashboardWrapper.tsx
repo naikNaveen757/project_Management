6cec6bfe62d07d4053520ef7a1943d968c12997a
emb-navenai <emb-navenai@embitel.com>
2024-09-24 17:50:24 +0530

    global state

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+"use client";
+
+import React, { Children, useEffect } from "react";
 import Navbar from "@/app/components/Navbar/page";
 import Sidebar from "@/app/components/Sidebar/index";
+import StoreProvider, { useAppSelector } from "./redux";
 
+const DashboardLayout = ({ Children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-const DashboardWrapper = ({ Children }:{children: React.ReactNode}) => {
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-        {/* slidebar */}
-        <Sidebar/>
-        slidebar
-            <main className={'flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'}>
-            {/* Navbar */}
-            <Navbar/>
-            {Children}
-            </main>
-
+      {/* slidebar */}
+      <Sidebar />
+      slidebar
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "" : "md:pl-64m"}`}
+      >
+        {/* Navbar */}
+        <Navbar />
+        {Children}
+      </main>
     </div>
+  );
+};
 
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children} </DashboardLayout>
+    </StoreProvider>
   );
 };
 
